8e8004a7de94e170ce79d6707c4b2c085199cd49
Albert Arvidsson <albert.arvidsson@gmail.com>
2019-04-19 00:48:25 +0200

    Updates

---
+++ precache-manifest.4338be0cfa363531749b6a78cffeb658.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5a41ec9a34d0b76e0d82",
+    "url": "/first-name-decider/static/js/runtime~main.e6e55250.js"
+  },
+  {
+    "revision": "7688d7323215fe5f9e4f",
+    "url": "/first-name-decider/static/js/main.154ad444.chunk.js"
+  },
+  {
+    "revision": "71a7ee0194b4d09a6441",
+    "url": "/first-name-decider/static/js/2.00826b61.chunk.js"
+  },
+  {
+    "revision": "7688d7323215fe5f9e4f",
+    "url": "/first-name-decider/static/css/main.4b6aab29.chunk.css"
+  },
+  {
+    "revision": "f926ee0d8df2a169638c64ba9484528e",
+    "url": "/first-name-decider/index.html"
+  }
+];
