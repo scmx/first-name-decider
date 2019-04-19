e0db98ccf7caaa6a3be02fdb31f4a75196b564c4
Albert Arvidsson <albert.arvidsson@gmail.com>
2019-04-19 07:48:50 +0200

    Improve name generator to not start on same letter

--- src/nameGenerator.js
+++ src/nameGenerator.js
@@ -2,10 +2,15 @@ function randomName(names) {
   return names[Math.floor(Math.random() * names.length)];
 }
 
+function doesNotStartsWithSameLetterAs(otherName) {
+  return name => name[0] !== otherName[0];
+}
+
 export function nameGenerator(names, ...current) {
   const second = randomName(names);
-  const rest = names.slice();
+  let rest = names.slice();
   rest.splice(rest.indexOf(second), 1);
+  rest = rest.filter(doesNotStartsWithSameLetterAs(second));
   const third = randomName(rest);
 
   return [second, third];
